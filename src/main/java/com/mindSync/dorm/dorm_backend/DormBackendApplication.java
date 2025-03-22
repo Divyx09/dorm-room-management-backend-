78c07b417226c9c985a3821a489bef9e5a35dad8
Divyx09 <divyaattarde94@gmail.com>
2025-03-22 11:26:38 +0530

    shree ganeshaya namaha:

---
+++ src/main/java/com/mindSync/dorm/dorm_backend/DormBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindSync.dorm.dorm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindSync/dorm/dorm_backend/DormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindSync.dorm.dorm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
