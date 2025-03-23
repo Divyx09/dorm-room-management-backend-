e3021dbf42e0512c3449b2318d49017778bdce79
Sameerdev512 <sameerkhatridev512@gmail.com>
2025-03-23 09:56:12 +0530

    Funcationalities added

--- src/main/java/com/mindSync/dorm/dorm_backend/controller/AuthController.java
+++ src/main/java/com/mindSync/dorm/dorm_backend/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://192.168.241.153")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final AuthService authService;
--- src/main/java/com/mindSync/dorm/dorm_backend/controller/UserController.java
+++ src/main/java/com/mindSync/dorm/dorm_backend/controller/UserController.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     final UserService userService;
